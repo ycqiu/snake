9e12645139317b76cfd456e2a48ef7c25f86955b
ycqiu <429148848@qq.com>
2016-04-30 21:42:31 +0800

    启用新ai

--- main.cpp
+++ main.cpp
@@ -56,7 +56,8 @@ int main()
 	CSnake game;
 	game.printPlat();
 	//CAi ai(game);
-	CCleverAi ai(game);
+	//CCleverAi ai(game);
+	CNormalAi ai(game);
 	while(ret >= 0)
 	{
 		ret = game.draw();
@@ -105,8 +106,8 @@ int main()
 			INFO_LOG("go ret: %d, snake eat all apple", ret);
 			break;
 		}
-		Sleep(300);
-	//	Sleep(50);
+		//Sleep(300);
+		Sleep(50);
 	}
 	return 0;
 }
